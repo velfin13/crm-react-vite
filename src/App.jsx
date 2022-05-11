62d568bead1e98dad5ed8f5f98934b206985c4ed
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-05-11 14:45:44 -0500

    sistema de routas terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./layouts/Login";
 import Layout from "./layouts/Layout";
 import Inicio from "./pages/Inicio";
-import FormularioLogin from "./pages/FormularioLogin";
+import NuevoCliente from "./pages/NuevoCliente";
+import EditarCliente from "./pages/EditarCliente";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}>
-          <Route index element={<FormularioLogin />} />
-        </Route>
+    
         <Route path="/clientes" element={<Layout />}>
           <Route index element={<Inicio />} />
+          <Route path="nuevo" element={<NuevoCliente />} />
+          <Route path="editar/:id" element={<EditarCliente />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/FormularioLogin.jsx
+++
@@ -1,9 +0,0 @@
-const FormularioLogin = () => {
-  return (
-    <div>
-      <h1>FormularioLogin</h1>
-    </div>
-  );
-};
-
-export default FormularioLogin;
