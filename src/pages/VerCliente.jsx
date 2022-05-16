6697197a6ee7a024eab2b4c3eea5576cbb303dd6
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-05-16 11:25:01 -0500

    validando cuando no hay datos

--- src/pages/VerCliente.jsx
+++ src/pages/VerCliente.jsx
@@ -3,10 +3,9 @@ import { useParams } from "react-router-dom";
 import { getClientByIdAPI } from "../api/clientes";
 
 const VerCliente = () => {
-  const parametros = useParams();
+  const { id } = useParams();
   const [cliente, setCliente] = useState({});
   const [cargando, setCargando] = useState(false);
-  const { id } = parametros;
 
   useEffect(() => {
     setCargando(true);
@@ -19,43 +18,57 @@ const VerCliente = () => {
   console.log(Object.keys(cliente) === 0);
 
   return (
-    <>
-      <h1 className="font-black text-4xl text-blue-900">
-        Detalles del Cliente
-      </h1>
-      <div className="mt-8">
-        <p className="text-2xl text-gray-700">
-          <span className="uppercase font-bold text-gray-800">Cliente: </span>{" "}
-          {cliente.nombre}
-        </p>
+    <div>
+      {cargando ? (
+        <p>Cargando ...</p>
+      ) : Object.keys(cliente).length === 0 ? (
+        <p className="p-2 bg-yellow-500 rounded-md shadow-md text-center text-white font-bold mt-5">No hay Resultados</p>
+      ) : (
+        <>
+          <h1 className="font-black text-4xl text-blue-900">
+            Detalles del Cliente
+          </h1>
+          <div className="mt-8">
+            <p className="text-2xl text-gray-700">
+              <span className="uppercase font-bold text-gray-800">
+                Cliente:{" "}
+              </span>{" "}
+              {cliente.nombre}
+            </p>
 
-        <p className="text-2xl text-gray-700">
-          <span className="uppercase font-bold text-gray-800">Email: </span>{" "}
-          {cliente.email}
-        </p>
+            <p className="text-2xl text-gray-700">
+              <span className="uppercase font-bold text-gray-800">Email: </span>{" "}
+              {cliente.email}
+            </p>
 
-        <p className="text-2xl text-gray-700">
-          <span className="uppercase font-bold text-gray-800">Empresa: </span>{" "}
-          {cliente.empresa}
-        </p>
+            <p className="text-2xl text-gray-700">
+              <span className="uppercase font-bold text-gray-800">
+                Empresa:{" "}
+              </span>{" "}
+              {cliente.empresa}
+            </p>
 
-        {cliente.telefono && (
-          <p className="text-2xl text-gray-700">
-            <span className="uppercase font-bold text-gray-800">
-              Telefono:{" "}
-            </span>{" "}
-            {cliente.telefono}
-          </p>
-        )}
+            {cliente.telefono && (
+              <p className="text-2xl text-gray-700">
+                <span className="uppercase font-bold text-gray-800">
+                  Telefono:{" "}
+                </span>{" "}
+                {cliente.telefono}
+              </p>
+            )}
 
-        {cliente.notas && (
-          <p className="text-2xl text-gray-700">
-            <span className="uppercase font-bold text-gray-800">Notas: </span>{" "}
-            {cliente.notas}
-          </p>
-        )}
-      </div>
-    </>
+            {cliente.notas && (
+              <p className="text-2xl text-gray-700">
+                <span className="uppercase font-bold text-gray-800">
+                  Notas:{" "}
+                </span>{" "}
+                {cliente.notas}
+              </p>
+            )}
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
